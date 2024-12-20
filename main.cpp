f532387339530262d6b8a3522fc9cd747fbc4478
aryafirmansyah2 <aryafirmansyah200404@example.com>
2024-12-20 20:06:21 +0700

    first commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <chrono> // Untuk pengukuran waktu
+#include <chrono>
 
 using namespace std;
 
@@ -11,7 +11,6 @@ struct Karyawan
   double pajak;
 };
 
-// Fungsi Rekursif
 double hitungGajiRekursif(Karyawan *karyawanList, int index, int totalKaryawan)
 {
   if (index == totalKaryawan)
@@ -25,7 +24,6 @@ double hitungGajiRekursif(Karyawan *karyawanList, int index, int totalKaryawan)
   }
 }
 
-// Fungsi Iteratif
 double hitungGajiIteratif(Karyawan *karyawanList, int totalKaryawan)
 {
   double totalGaji = 0;
@@ -38,27 +36,24 @@ double hitungGajiIteratif(Karyawan *karyawanList, int totalKaryawan)
 
 int main()
 {
-  // Ukuran data yang berbeda untuk menguji performa
-  const int jumlahKaryawan = 1000;                       // Menentukan jumlah karyawan
-  Karyawan *karyawanList = new Karyawan[jumlahKaryawan]; // Alokasi array dinamis
 
-  // Mengisi data karyawan
+  const int jumlahKaryawan = 1000;
+  Karyawan *karyawanList = new Karyawan[jumlahKaryawan];
+
   for (int i = 0; i < jumlahKaryawan; i++)
   {
-    // Menghasilkan nilai acak dalam rentang yang diinginkan
-    double gajiPokok = 20000; // Rentang 2.000.000 - 10.000.000
-    double tunjangan = 5000;  // Rentang 500.000 - 5.500.000
-    double potongan = 1000;   // Rentang 100.000 - 2.100.000
-    double pajak = 1000;      // Rentang 100.000 - 1.100.000
 
-    // Menyimpan data yang sudah diacak ke dalam array karyawan
+    double gajiPokok = 20000;
+    double tunjangan = 5000;
+    double potongan = 1000;
+    double pajak = 1000;
+
     karyawanList[i].gajiPokok = gajiPokok;
     karyawanList[i].tunjangan = tunjangan;
     karyawanList[i].potongan = potongan;
     karyawanList[i].pajak = pajak;
   }
 
-  // Pengukuran waktu untuk fungsi rekursif
   auto start = chrono::high_resolution_clock::now();
   double totalGajiRekursif = hitungGajiRekursif(karyawanList, 0, jumlahKaryawan);
   auto end = chrono::high_resolution_clock::now();
@@ -66,7 +61,6 @@ int main()
   cout << "Total Gaji Semua Karyawan (Rekursif): Rp " << totalGajiRekursif << endl;
   cout << "Waktu Eksekusi Rekursif untuk " << jumlahKaryawan << " karyawan: " << rekursifDuration.count() << " detik" << endl;
 
-  // Pengukuran waktu untuk fungsi iteratif
   start = chrono::high_resolution_clock::now();
   double totalGajiIteratif = hitungGajiIteratif(karyawanList, jumlahKaryawan);
   end = chrono::high_resolution_clock::now();
@@ -74,7 +68,7 @@ int main()
   cout << "Total Gaji Semua Karyawan (Iteratif): Rp " << totalGajiIteratif << endl;
   cout << "Waktu Eksekusi Iteratif untuk " << jumlahKaryawan << " karyawan: " << iteratifDuration.count() << " detik" << endl;
 
-  delete[] karyawanList; // Jangan lupa untuk membebaskan memori yang dialokasikan
+  delete[] karyawanList;
 
   return 0;
 }
